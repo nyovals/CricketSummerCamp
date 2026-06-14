2b45675e716ef16dcccb45c1c17fdbf293f33d8f
nyovals <gatparajini@gmail.com>
2026-06-14 10:40:58 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,46 @@
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("nyovals-cache").then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/icons/NYOvalsLogo.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// INSTALL EVENT
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("nyovals-cache").then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/images/NYOvalsLogo.png",
+        "/images/image1.jpg",
+        "/images/image2.jpg",
+        "/images/image3.jpg",
+        "/images/image4.jpg"
+      ]);
+    })
+  );
+});
+
+// FETCH EVENT
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
