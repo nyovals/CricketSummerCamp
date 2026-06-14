0c3c67517fc9d537bb74f9aa0651b47411acbc65
nyovals <gatparajini@gmail.com>
2026-06-14 10:53:00 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,15 @@
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open("nyovals-cache").then(cache => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/icons/NYOvalsLogo.png"
-      ]);
-    })
-  );
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
 // INSTALL EVENT
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open("nyovals-cache").then(cache => {
       return cache.addAll([
-        "/",
-        "/index.html",
-        "/images/NYOvalsLogo.png",
-        "/images/image1.jpg",
-        "/images/image2.jpg",
-        "/images/image3.jpg",
-        "/images/image4.jpg"
+        "./",
+        "index.html",
+        "images/NYOvalsLogo.png",
+        "images/image1.jpg",
+        "images/image2.jpg",
+        "images/image3.jpg",
+        "images/image4.jpg"
       ]);
     })
   );
@@ -43,4 +23,3 @@ self.addEventListener("fetch", event => {
     })
   );
 });
-
